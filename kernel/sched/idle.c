f017f9bd66d44333f5eb0d829eade68c21c0e0da
Demon000 <demonsingur@gmail.com>
2017-04-15 23:14:57 +0300

    clk: cpu-8994: fix for upstream

--- kernel/sched/idle.c
+++ kernel/sched/idle.c
@@ -37,6 +37,24 @@ void cpu_idle_poll_ctrl(bool enable)
 	}
 }
 
+static DEFINE_PER_CPU(int, idle_force_poll);
+
+void per_cpu_idle_poll_ctrl(int cpu, bool enable)
+{
+	if (enable) {
+		per_cpu(idle_force_poll, cpu)++;
+	} else {
+		per_cpu(idle_force_poll, cpu)--;
+		WARN_ON_ONCE(per_cpu(idle_force_poll, cpu) < 0);
+	}
+
+	/*
+	 * Make sure poll mode is entered on the relevant CPU after the flag is
+	 * set
+	 */
+	mb();
+}
+
 #ifdef CONFIG_GENERIC_IDLE_POLL_SETUP
 static int __init cpu_idle_poll_setup(char *__unused)
 {
